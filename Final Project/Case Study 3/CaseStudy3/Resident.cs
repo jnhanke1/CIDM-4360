75df428605eda58756254c9cf6811b9bdd57e706
Jessica Hanke <jnhanke1@buffs.wtamu.edu>
2023-11-27 16:35:59 -0600

    clearing commit file

---
+++ Final Project/Case Study 3/CaseStudy3/Resident.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+class Resident{
+    public int id; 
+    public varchar(30) full_name; 
+    public EmailAddressAttribute email; 
+    public int unit_number; 
+
+}
