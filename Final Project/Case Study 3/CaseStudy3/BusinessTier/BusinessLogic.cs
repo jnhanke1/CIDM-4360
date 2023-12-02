0021de93ae3d19ccc254f66006ffbcd7dfee8641
Jessica Hanke <jnhanke1@buffs.wtamu.edu>
2023-12-01 21:42:28 -0600

    jkfdsjakl

--- Final Project/Case Study 3/CaseStudy3/BusinessTier/BusinessLogic.cs
+++ Final Project/Case Study 3/CaseStudy3/BusinessTier/BusinessLogic.cs
@@ -15,6 +15,7 @@ class BusinessLogic
         DataTier database = new DataTier();
         Resident resident; 
         Package tracking_number; 
+        Package recipient_name; 
 
         // start GUI
         staff_username = appGUI.Login();
@@ -52,20 +53,20 @@ class BusinessLogic
                     // Notate a Package Picked Up
                     case 4:
                         Console.WriteLine("Notate a Package Picked Up");
-
+                        database.PickupPackage(tracking_number); 
                         break;
                     // Delete a Package
                     case 5:
                         Console.WriteLine("Delete a Package");
-                        database.deletePackage(); 
+                        database.deletePackage(tracking_number); 
                         break;      
                     // Search Package History
                     case 6:
                         Console.WriteLine("Search Package History");
-
+                        database.searchPackage(recipient_name);
                         break; 
                     case 7:
-                    //     Console.WriteLine("See Pending Packages");
+                        Console.WriteLine("See Pending Packages");
                     //    DataTable Pending_Packages(DataTable tablePending_Packages){
                     //     Console.WriteLine("-------------------- List of all Pending Packages --------------------"); 
                     //     foreach(DataRow row in tablePending_Packages.Rows){
@@ -89,13 +90,15 @@ class BusinessLogic
                         Console.WriteLine("Wrong Input");
                         break;}
                 }
-            }
+            
         
         }
 
         else{
                 Console.WriteLine("Login Failed, Goodbye.");
         }        
-    }   }
+    }   
+
+}
   
 
